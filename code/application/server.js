8a94bf5d303003a88c91eb9f60c941fb7436d032
Manuel Kiessling <manuel@kiessling.net>
2011-04-18 09:07:35 +0200

    finished chapter on how to make your own modules

--- code/application/server.js
+++ code/application/server.js
@@ -1,13 +1,16 @@
 var sys = require("sys");
 var http = require("http");
 
-function handle(request, response) {
-  sys.puts("Request received.");
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("Hello World");
-  response.end();
+function start() {
+  function handle(request, response) {
+    sys.puts("Request received.");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("Hello World");
+    response.end();
+  }
+
+  http.createServer(handle).listen(8888);
+  sys.puts("Server has started.");
 }
 
-http.createServer(handle).listen(8888);
-
-sys.puts("Server has started.");
+exports.start = start;
