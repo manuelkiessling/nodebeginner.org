ed56a2f9f54f8b32d0b5f011085a653348e5510e
Manuel Kiessling <manuel@kiessling.net>
2011-04-23 08:51:34 +0200

    fixed error in router, wrong comparison. Hat tip to Tarun and crodway for pointing it out!

--- code/application/router.js
+++ code/application/router.js
@@ -1,6 +1,6 @@
 function route(handle, pathname) {
   console.log("About to route a request for " + pathname);
-  if (typeof handle[pathname] !== 'function') {
+  if (typeof handle[pathname] === 'function') {
     return handle[pathname]();
   } else {
     console.log("No request handler found for " + pathname);
