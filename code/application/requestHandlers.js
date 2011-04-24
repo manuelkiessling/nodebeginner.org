abfbced5db076e3b9b651b0811677b4f9704d77d
Manuel Kiessling <manuel@kiessling.net>
2011-04-24 12:53:16 +0200

    finishing current update

--- code/application/requestHandlers.js
+++ code/application/requestHandlers.js
@@ -3,11 +3,13 @@ var exec = require("child_process").exec;
 function start(response) {
   console.log("Request handler 'start' was called.");
 
-  exec("find /", { timeout: 10000, maxBuffer: 20000*1024 }, function (error, stdout, stderr) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(stdout);
-    response.end();
-  });
+  exec("find /",
+    { timeout: 10000, maxBuffer: 20000*1024 },
+    function (error, stdout, stderr) {
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.write(stdout);
+      response.end();
+    });
 }
 
 function upload(response) {
