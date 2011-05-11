f9f648d492da2aa2314f634ff1cb88362f65bea7
Manuel Kiessling <manuel@kiessling.net>
2011-05-11 10:07:41 +0200

    added HTML upload form to /start request handler

--- code/application/requestHandlers.js
+++ code/application/requestHandlers.js
@@ -3,13 +3,14 @@ var exec = require("child_process").exec;
 function start(response) {
   console.log("Request handler 'start' was called.");
 
-  exec("find /",
-    { timeout: 10000, maxBuffer: 20000*1024 },
-    function (error, stdout, stderr) {
-      response.writeHead(200, {"Content-Type": "text/html"});
-      response.write(stdout);
-      response.end();
-    });
+  var body = '<form action="/upload" method="post" enctype="multipart/form-data">'+
+    '<input type="file" name="upload-file">'+
+    '<input type="submit" value="Upload">'+
+    '</form>'
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
 }
 
 function upload(response) {
