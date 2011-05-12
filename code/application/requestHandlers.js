59ea120129d75326c62f5a11debec2041ed71cf3
Manuel Kiessling <manuel@kiessling.net>
2011-05-12 10:11:01 +0200

    serving a full html doc for /start, including utf-8 encoding, explained how to integrate the request data handling into the server

--- code/application/requestHandlers.js
+++ code/application/requestHandlers.js
@@ -3,10 +3,17 @@ var exec = require("child_process").exec;
 function start(response) {
   console.log("Request handler 'start' was called.");
 
-  var body = '<form action="/upload" method="post">'+
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
     '<textarea name="text" rows="20" cols="60"></textarea>'+
     '<input type="submit" value="Submit text" />'+
-    '</form>'
+    '</form>'+
+    '</body>'+
+    '</html>';
 
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write(body);
--- code/application/server.js
+++ code/application/server.js
@@ -3,10 +3,21 @@ var url = require("url");
 
 function start(route, handle) {
   function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
+    var postData = "";
+	var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
 
-    route(handle, pathname, response);
+    request.setEncoding("utf8");
+
+    request.addListener("data", function(postDataChunk) {
+      postData += postDataChunk;
+      console.log("Received POST data chunk '" + postDataChunk + "'.");
+    });
+
+    request.addListener("end", function() {
+      route(handle, pathname, response, postData);
+    });
+
   }
 
   http.createServer(onRequest).listen(8888);
