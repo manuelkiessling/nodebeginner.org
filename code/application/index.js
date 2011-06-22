b4611d0e35f910da14d8f67e09c515d372c423ee
Manuel Kiessling <manuel@kiessling.net>
2011-06-22 09:35:42 +0200

    working towards the last three steps of the use case

--- code/application/index.js
+++ code/application/index.js
@@ -6,5 +6,6 @@ var handle = {}
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
 
 server.start(router.route, handle);
--- code/application/requestHandlers.js
+++ code/application/requestHandlers.js
@@ -1,4 +1,5 @@
-var querystring = require("querystring");
+var querystring = require("querystring"),
+    fs = require("fs");
 
 function start(response, postData) {
   console.log("Request handler 'start' was called.");
@@ -8,8 +9,8 @@ function start(response, postData) {
     '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    '<input type="file" name="upload">'+
     '<input type="submit" value="Submit text" />'+
     '</form>'+
     '</body>'+
@@ -27,5 +28,21 @@ function upload(response, postData) {
   response.end();
 }
 
+function show(response, postData) {
+  console.log("Request handler 'show' was called.");
+  fs.readFile("/tmp/test.png", "binary", function(error, file) {
+    if(error) {
+      response.writeHead(500, {"Content-Type": "text/plain"});
+      response.write(err + "\n");
+      response.end();
+    } else {
+      response.writeHead(200, {"Content-Type": "image/png"});
+      response.write(file, "binary");
+      response.end();
+    }
+  });
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
