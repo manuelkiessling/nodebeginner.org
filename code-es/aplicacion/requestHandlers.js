4a7801b632751ec8d5279a2fefdff732bd79494d
Herman A. Junge <haj@neosource.cl>
2011-06-28 16:53:58 -0400

    SP Trans mark on 1036

--- code-es/aplicacion/requestHandlers.js
+++ code-es/aplicacion/requestHandlers.js
@@ -1,8 +1,14 @@
+// requestHandlers.js
+// Ejemplo de NodeBeginner
+// Se intenta traducir al máximo al Español,
+// sin embargo, con el fin de preservar el sentido, se dejan 
+// algunas variables en inglés.
+
 var querystring = require("querystring"),
     fs = require("fs"),
     formidable = require("formidable");
 
-function inicio(response) {
+function iniciar(response) {
   console.log("Manipulador de Peticion para 'inicio' fue llamado.");
 
   var body = '<html>'+
@@ -22,23 +28,23 @@ function inicio(response) {
     response.end();
 }
 
-function upload(response, request) {
-  console.log("Request handler 'upload' was called.");
+function subir(response, request) {
+  console.log("Manipulador de Peticion para 'subir' fue llamado.");
 
   var form = new formidable.IncomingForm();
-  console.log("about to parse");
+  console.log("A punto de parsear");
   form.parse(request, function(err, fields, files) {
-    console.log("parsing done");
+    console.log("parseo hecho");
     fs.renameSync(files.upload.path, "/tmp/test.png");
     response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("received image:<br/>");
+    response.write("imagen recibida:<br/>");
     response.write("<img src='/show' />");
     response.end();
   });
 }
 
-function show(response) {
-  console.log("Request handler 'show' was called.");
+function mostrar(response) {
+  console.log("Manipulador de Peticion para 'mostrar' fue llamado.");
   fs.readFile("/tmp/test.png", "binary", function(error, file) {
     if(error) {
       response.writeHead(500, {"Content-Type": "text/plain"});
@@ -46,12 +52,12 @@ function show(response) {
       response.end();
     } else {
       response.writeHead(200, {"Content-Type": "image/png"});
-      response.write(file, "binary");
+      response.write(file, "binario");
       response.end();
     }
   });
 }
 
-exports.start = start;
-exports.upload = upload;
-exports.show = show;
+exports.iniciar = iniciar;
+exports.subir = subir;
+exports.mostrar = mostrar;
