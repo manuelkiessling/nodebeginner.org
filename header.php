fc8a94d7ff63faab3636a180d88edcb6b1e8e7ca
Manuel Kiessling <manuel@kiessling.net>
2011-10-09 14:41:40 +0200

    extensive page/site performance optimizations

---
+++ header.php
@@ -0,0 +1,30 @@
+<?php
+
+ob_start();
+
+$pathinfo = pathinfo($_ENV['SCRIPT_FILENAME']);
+$extension = $pathinfo['extension'];
+
+if ($extension != 'html') {
+    header('Cache-Control: max-age=604800'); // 1 week
+}
+
+if ($extension == 'css') {
+    header('Content-type: text/css; charset=utf-8');
+}
+
+if ($extension == 'js') {
+    header('Content-type: text/javascript; charset=utf-8');
+}
+
+if ($extension == 'png') {
+    header('Content-type: image/png');
+}
+
+if ($extension == 'jpg') {
+    header('Content-type: image/jpeg');
+}
+
+if ($extension == 'gif') {
+    header('Content-type: image/gif');
+}
