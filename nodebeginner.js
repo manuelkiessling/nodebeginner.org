8aa21aaf2e4a647e2335e76390ac99161230311b
Manuel Kiessling <manuel@kiessling.net>
2011-10-10 00:02:14 +0200

    loading google analytics after finished page load

--- nodebeginner.js
+++ nodebeginner.js
@@ -28,8 +28,9 @@ var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-2127388-6']);
 _gaq.push(['_trackPageview']);
 
-(function loadAnalytics() {
+function loadAnalytics() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+}
+setTimeout(loadAnalytics, 1);
