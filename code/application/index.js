0570931caf2eda4dfa7651a4abc305a111f3142c
Manuel Kiessling <manuel@kiessling.net>
2014-08-27 18:51:58 +0200

    added missing semicolons to code, added google identification page, google adwords remarketing tag

--- code/application/index.js
+++ code/application/index.js
@@ -2,7 +2,7 @@ var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 
-var handle = {}
+var handle = {};
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
