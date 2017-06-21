867aee1148e2b313b70ed8bfe420a62f765d70fc
Manuel Kiessling <manuel@kiessling.net>
2017-06-21 10:09:26 +0200

    Centralizing default headers.

---
+++ _defaultheaders.php
@@ -0,0 +1,3 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+<link rel="icon" href="favicon.png" type="image/png" />
