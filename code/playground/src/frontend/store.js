d1d4833da762801a931b6c417a56f9d01889e531
Manuel Kießling <manuel@kiessling.net>
2018-08-22 05:42:24 +0200

    Code style.

--- code/playground/src/frontend/store.js
+++ code/playground/src/frontend/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
-import thunkMiddleware from 'redux-thunk'
+import thunkMiddleware from "redux-thunk";
 import rootReducer from "./reducers";
 
 const store = createStore(
