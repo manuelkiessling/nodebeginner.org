3183f552655dd19ca1f53c991c70512c654b34ee
Manuel Kießling <manuel@kiessling.net>
2018-08-23 15:30:53 +0200

    Fixing intendation.

--- code/playground/webpack.config.js
+++ code/playground/webpack.config.js
@@ -1,43 +1,43 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: [
-            "/node_modules/",
-            "/src/backend/"
-          ],
-          use: {
-            loader: "babel-loader"
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [
+          "/node_modules/",
+          "/src/backend/"
+        ],
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
           }
-        },
-        {
-          test: /\.html$/,
-          use: [
-            {
-              loader: "html-loader"
-            }
-          ]
-        }
-      ]
-    },
-    plugins: [
-      new HtmlWebPackPlugin({
-        template: "./src/frontend/index.html",
-        filename: "./index.html"
-      })
-    ],
-    entry: './src/frontend/index.js',
-    devtool: 'inline-source-map',
-    devServer: {
-      contentBase: './dist',
-      proxy: {
-        '/api': {
-            target: 'http://localhost:3000',
-            secure: false
-        }
-      }  
-    }
-  };
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/frontend/index.html",
+      filename: "./index.html"
+    })
+  ],
+  entry: './src/frontend/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false
+      }
+    }  
+  }
+};
