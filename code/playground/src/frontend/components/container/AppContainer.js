4a9d43e837e59327bc3ef25e12e0beed4aacdeec
Manuel Kießling <manuel@kiessling.net>
2018-08-29 15:30:30 +0200

    Integrated Material UI CSS Baseline.

--- code/playground/src/frontend/components/container/AppContainer.js
+++ code/playground/src/frontend/components/container/AppContainer.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import ConnectedListContainer from "./ListContainer";
 import ConnectedFormContainer from "./FormContainer";
 import { fetchArticlesThunk } from "../../actions/thunks"
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const mapStateToProps = (state) => {
     return {
@@ -19,20 +20,23 @@ const mapDispatchToProps = (dispatch) => {
 class AppContainer extends Component {
     render() {
         return (
-            <div className="row mt-5">
-                <div className="col-md-4 offset-md-1">
-                    <h2>Articles</h2>
-                    <button type="submit" className="btn btn-success btn-lg" onClick={this.props.dispatchFetchArticles}>
-                        FETCH
-                    </button>
-                    <ConnectedListContainer />
-                    <pre>{JSON.stringify(this.props.debugInfo)}</pre>
+            <React.Fragment>
+                <CssBaseline />
+                <div className="row mt-5">
+                    <div className="col-md-4 offset-md-1">
+                        <h2>Articles</h2>
+                        <button type="submit" className="btn btn-success btn-lg" onClick={this.props.dispatchFetchArticles}>
+                            FETCH
+                        </button>
+                        <ConnectedListContainer />
+                        <pre>{JSON.stringify(this.props.debugInfo)}</pre>
+                    </div>
+                    <div className="col-md-4 offset-md-1">
+                        <h2>Add a new article</h2>
+                        <ConnectedFormContainer />
+                    </div>
                 </div>
-                <div className="col-md-4 offset-md-1">
-                    <h2>Add a new article</h2>
-                    <ConnectedFormContainer />
-                </div>
-            </div>
+            </React.Fragment>
         );
     }
 }
