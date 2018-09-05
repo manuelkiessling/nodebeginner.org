d153c82739aecc378b50bd47bc4a5d74df92da73
Manuel Kießling <manuel@kiessling.net>
2018-09-05 19:43:45 +0200

    WIP: Towards a real task management application.
    Cleanups.

--- code/playground/src/universal/react-components/presentational/TaskList.js
+++ code/playground/src/universal/react-components/presentational/TaskList.js
@@ -1,13 +1,16 @@
 import React from "react";
 import MuiList from "@material-ui/core/List";
 import TaskListItem from "./TaskListItem";
+import MuiPaper from "./mui-overrides/Paper";
 
 const TaskList = ({ tasks }) => (
-    <MuiList>
-        {tasks.map(tasks => (
-            <TaskListItem key={tasks.id} task={tasks} />
-        ))}
-    </MuiList>
+    <MuiPaper>
+        <MuiList>
+            {tasks.map(tasks => (
+                <TaskListItem key={tasks.id} task={tasks} />
+            ))}
+        </MuiList>
+    </MuiPaper>
 );
 
 export default TaskList;
