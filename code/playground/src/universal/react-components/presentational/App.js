3089497eff9b83d4606d5de64c769cdfaf94668f
Manuel Kießling <manuel@kiessling.net>
2018-11-22 16:54:26 +0100

    Playground: App Bar.

--- code/playground/src/universal/react-components/presentational/App.js
+++ code/playground/src/universal/react-components/presentational/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import MuiCssBaseline from "@material-ui/core/CssBaseline";
+import MuiAppBar from '@material-ui/core/AppBar';
+import MuiToolbar from '@material-ui/core/Toolbar';
+import MuiTypography from '@material-ui/core/Typography';
 import { Route, Switch } from "react-router-dom";
 import routes from "../../routes";
 
@@ -7,6 +10,14 @@ const App = () => (
     <React.Fragment>
 
         <MuiCssBaseline />
+
+        <MuiAppBar position="static" color="primary">
+            <MuiToolbar>
+                <MuiTypography variant="h6" color="inherit">
+                    Notes app
+                </MuiTypography>
+            </MuiToolbar>
+        </MuiAppBar>
 
         <Switch>
             { routes.map( route => <Route key={ route.path } { ...route } /> ) }
