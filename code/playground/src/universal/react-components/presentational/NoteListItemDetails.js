959096a2cbf65a12002e4e187e00ed5b15dbc3ce
Manuel Kießling <manuel@kiessling.net>
2018-11-29 21:20:54 +0100

    Playground: Layouting.

--- code/playground/src/universal/react-components/presentational/NoteListItemDetails.js
+++ code/playground/src/universal/react-components/presentational/NoteListItemDetails.js
@@ -1,20 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 import MuiTypography from "@material-ui/core/Typography";
+import MuiPaper from "@material-ui/core/es/Paper/Paper";
+import { withStyles } from "@material-ui/core/styles";
 import { NoteEntity } from "../../entities/NoteEntity";
 
-const NoteListItemDetails = ({ note }) => (
-    <MuiTypography variant="caption" color="textSecondary">
-        {note.id}
-        -
-        {note.title}
-        -
-        {note.lastModified}
-    </MuiTypography>
+const styles = (theme) => ({
+    root: {
+        padding: theme.spacing.unit * 4,
+        minHeight: "100%"
+    },
+});
+
+const NoteListItemDetails = ({ note, classes }) => (
+    <MuiPaper className={classes.root}>
+        <MuiTypography variant="caption" color="textSecondary">
+            {note.id}
+            -
+            {note.title}
+            -
+            {note.lastModified}
+        </MuiTypography>
+    </MuiPaper>
 );
 
 NoteListItemDetails.propTypes = {
     note: PropTypes.instanceOf(NoteEntity)
 };
 
-export default NoteListItemDetails;
+export default withStyles(styles)(NoteListItemDetails);
