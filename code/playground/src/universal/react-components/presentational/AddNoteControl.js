3f45490b5b61dcd2c67a27d23a1e42a428cc2d84
Manuel Kießling <manuel@kiessling.net>
2018-11-29 09:13:49 +0100

    Playground: Layouting.

--- code/playground/src/universal/react-components/presentational/AddNoteControl.js
+++ code/playground/src/universal/react-components/presentational/AddNoteControl.js
@@ -1,21 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 import MuiButton from "@material-ui/core/Button";
-import MuiFormControl from "@material-ui/core/FormControl";
-import MuiInputLabel from "@material-ui/core/InputLabel";
-import MuiInput from "@material-ui/core/Input";
+import MuiTextField from "@material-ui/core/TextField";
+import MuiGrid from "@material-ui/core/Grid/Grid";
 
 const AddNoteControl = ({ handleSubmit, handleChange, title }) => (
     <form onSubmit={handleSubmit}>
-        <MuiFormControl margin="normal">
-            <MuiInputLabel htmlFor="title">Title</MuiInputLabel>
-            <MuiInput id="title" value={title} onChange={handleChange} />
-        </MuiFormControl>
-        <MuiFormControl>
-            <MuiButton type="submit" variant="contained" color="primary">
-                plus
-            </MuiButton>
-        </MuiFormControl>
+        <MuiGrid container direction="row" spacing={24} justify="flex-start" alignItems="center">
+            <MuiGrid item xs={10}>
+                <MuiTextField id="title" label="New note title" variant="outlined" value={title} onChange={handleChange} fullWidth />
+            </MuiGrid>
+            <MuiGrid item>
+                <MuiButton type="submit" variant="contained" color="primary">
+                    plus
+                </MuiButton>
+            </MuiGrid>
+        </MuiGrid>
     </form>
 );
 
