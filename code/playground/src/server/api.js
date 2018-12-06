177a0aac10534f13664ef7ae7f1689c639818ff8
Manuel Kießling <manuel@kiessling.net>
2018-12-06 14:10:21 +0100

    Playground: Refactoring.

--- code/playground/src/server/api.js
+++ code/playground/src/server/api.js
@@ -5,7 +5,7 @@ export default (server) => {
     console.info("API support at /api activated.");
     server.get(/^\/api\/entity-events\/$/, (req, res) => {
         res.writeHead(200, {"Content-Type": "application/json"});
-        fs.readFile(path.join(__dirname, "..", "src", "server", "mock-api-db.json"), "utf8", (err, data) => {
+        fs.readFile(path.join(__dirname, "..", "resources", "mock-api-db.json"), "utf8", (err, data) => {
             if (err) throw err;
             res.end(data);
         });
