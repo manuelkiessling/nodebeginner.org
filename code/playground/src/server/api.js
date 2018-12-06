41709ce3a5119f0947be243bf8f8e6a561fc276d
Manuel Kießling <manuel@kiessling.net>
2018-12-06 12:56:35 +0100

    Playground: Improving API code.

--- code/playground/src/server/api.js
+++ code/playground/src/server/api.js
@@ -1,9 +1,13 @@
-import mockApiContent from "./mock-api-db";
+import fs from "fs";
+import path from "path";
 
 export default (server) => {
     console.info("API support at /api activated.");
-    server.get(/^\/api(\?.*)*$/, (req, res) => {
+    server.get(/^\/api\/entity-events\/$/, (req, res) => {
         res.writeHead(200, {"Content-Type": "application/json"});
-        res.end(JSON.stringify(mockApiContent));
+        fs.readFile(path.join(__dirname, "..", "src", "server", "mock-api-db.json"), "utf8", (err, data) => {
+            if (err) throw err;
+            res.end(data);
+        });
     });
 }
