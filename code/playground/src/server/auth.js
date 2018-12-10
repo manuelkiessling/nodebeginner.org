5ebfa159842b30f190c3b6c8e8b722a4e1aff441
Manuel Kießling <manuel@kiessling.net>
2018-12-10 12:24:08 +0100

    Playground: WIP: authentication.

--- code/playground/src/server/auth.js
+++ code/playground/src/server/auth.js
@@ -25,46 +25,56 @@ const activateAuth = (server, callback) => {
         server.post(/^\/auth\/session-tokens\/$/, (req, res) => {
             console.info(`Received auth request: ${req.method} ${req.originalUrl}`);
 
-            if (   (req.body == null)
+            if (    (req.body == null)
+
                 || !(req.body.hasOwnProperty("username"))
-                || (req.body.username == null)
+                ||  (req.body.username == null)
                 || !(typeof req.body.username === "string")
-                || (req.body.username.length === 0)
+                ||  (req.body.username.length === 0)
+
                 || !(req.body.hasOwnProperty("password"))
-                || (req.body.password == null)
+                ||  (req.body.password == null)
                 || !(typeof req.body.password === "string")
-                || (req.body.password.length === 0)
+                ||  (req.body.password.length === 0)
             ) {
                 res.writeHead(400, { "Content-Type": "application/json" });
-                res.end(JSON.stringify({ error: "Invalid auth body." }));
+                res.end(JSON.stringify({ error: "Invalid authentication body." }));
                 return;
             }
 
             const username = req.body.username;
             const password = req.body.password;
-            const passwordHash = bcrypt.hashSync(password, 12);
-
-            accounts
-                .findOne({ username: username }, {}, (err, doc) => {
-                    if (err) {
-                        console.error(err);
-                        res.writeHead(500, { "Content-Type": "application/json" });
-                        res.end(JSON.stringify({ error: err.message }));
-                    } else {
-                        if (doc != null && doc.hasOwnProperty("passwordHash") && doc.passwordHash != null) {
-                            if (bcrypt.compareSync(password, doc.passwordHash) === true) {
-
+            accounts.findOne({ username: username }, {}, (err, doc) => {
+                if (err) {
+                    console.error(err);
+                    res.writeHead(500, { "Content-Type": "application/json" });
+                    res.end(JSON.stringify({ error: err.message }));
+                } else {
+                    if (doc != null && doc.hasOwnProperty("passwordHash") && doc.passwordHash != null) {
+                        console.info(`Account with username ${username} exists, verifying password...`);
+                        bcrypt.compare(password, doc.passwordHash, (err, matched) => {
+                            if (matched === true) {
+                                console.info(`Password for account with username ${username} is valid, creating session token...`);
+                                res.writeHead(201, { "Content-Type": "application/json" });
+                                res.end(JSON.stringify(""));
                             } else {
-
+                                console.info(`Password for account with username ${username} is invalid.`);
+                                res.writeHead(403, { "Content-Type": "application/json" });
+                                res.end(JSON.stringify("Access denied."));
                             }
-                            res.end(JSON.stringify(doc.events));
-                        } else {
-                            console.info(`Account with username ${username} does not yet exist, creating...`);
+                        });
+                    } else {
+                        console.info(`Account with username ${username} does not yet exist, creating...`);
+
+                        bcrypt.hash(password, 12, (passwordHash) => {
+
+                        });
+
                             res.writeHead(200, { "Content-Type": "application/json" });
-                            res.end(JSON.stringify([]));
-                        }
+                        res.end(JSON.stringify([]));
                     }
-                });
+                }
+            });
 
         });
 
